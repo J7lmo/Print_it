1368cc5f8f53c91a3a8a51e65c685ef65542834c
julien Lacroix <102021078+J7lmo@users.noreply.github.com>
2023-10-23 08:46:29 +0200

    Add updateTagline

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,33 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		"image": "slide1.jpg",
+		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		"image": "slide2.jpg",
+		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		"image": "slide3.jpg",
+		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		"image": "slide4.png",
+		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
-]
+];
+
 var fleche_gauche = document.querySelector(".arrow_left");
 var fleche_droite = document.querySelector(".arrow_right");
-var dots = document.querySelector(".dots"); 
-const img = document.querySelectorAll(".banner-img"); 
-const nbSlide = img.length
+var dots = document.querySelector(".dots");
+const img = document.querySelectorAll(".banner-img");
+const nbSlide = img.length;
 let i = 0;
+
+function updateTagline() {
+	const tagline = document.querySelector(".tagline");
+	tagline.innerHTML = slides[i].tagLine;
+}
 
 
 
@@ -31,36 +37,42 @@ for (let i = 0; i < slides.length; i++) {
 	dots.appendChild(Newdots);
 }
 
-const nbdot = document.querySelectorAll(".dot")
-nbdot[0].classList.add('dot_selected')
+const nbdot = document.querySelectorAll(".dot");
+nbdot[0].classList.add('dot_selected');
 
-function Slideprecedent(){
-	img[i].classList.remove('active')
-	nbdot[i].classList.remove('dot_selected')
+function Slideprecedent() {
+	img[i].classList.remove('active');
+	nbdot[i].classList.remove('dot_selected');
 
-	if(i != 0){
-		i --;
+	if (i != 0) {
+		i--;
 	} else {
 		i = nbSlide - 1;
 	}
-	img[i].classList.add('active')
-	nbdot[i].classList.add('dot_selected')
+
+	img[i].classList.add('active');
+	nbdot[i].classList.add('dot_selected');
+	updateTagline(); 
 	console.log(i);
 }
 
-function Slidesuivante(){
-	img[i].classList.remove('active')
-	nbdot[i].classList.remove('dot_selected')
+function Slidesuivante() {
+	img[i].classList.remove('active');
+	nbdot[i].classList.remove('dot_selected');
 
-	if(i < nbSlide - 1){
-		i ++;
+	if (i < nbSlide - 1) {
+		i++;
 	} else {
 		i = 0;
 	}
-	img[i].classList.add('active')
-	nbdot[i].classList.add('dot_selected')
+
+	img[i].classList.add('active');
+	nbdot[i].classList.add('dot_selected');
+	updateTagline(); 
 	console.log(i);
 }
 
-fleche_gauche.addEventListener("click",Slideprecedent)
-fleche_droite.addEventListener("click",Slidesuivante)
+fleche_gauche.addEventListener("click", Slideprecedent);
+fleche_droite.addEventListener("click", Slidesuivante);
+
+updateTagline();
